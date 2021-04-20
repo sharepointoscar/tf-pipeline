4a08e49cd2d9f0b7bda740e0a122e4fe9a82ef09
Oscar Medina <me@sharepointoscar.com>
2021-04-20 13:27:09 -0700

    feat: cleanup (#2)
    * WIP: terraform based pipeline definition
    
    * WIP: all resources properly deploying except webhook
    
    * pipeline fully working for sample static site on s3, need fix webhook
    
    * fully working pipeline uses terraform plan and apply.  see README for Issues
    
    * deployable pipeline, see readme for issues to tackle next
    
    * force destroy of s3 buckets when executing terraform destroy
    
    * adding pipeline architecture, more documentation, and removing unecessary ymls
    
    Co-authored-by: github-sharepointoscar <osmed@amazon.com>

--- s3.tf
+++ s3.tf
@@ -2,6 +2,8 @@ resource "aws_s3_bucket" "artifacts-bucket" {
   bucket = var.artifacts_bucket_name
   acl    = "private"
 
+  force_destroy = true
+
   tags = {
     Name        = "static-web-build-${var.env}"
     Environment = "Dev"
@@ -11,6 +13,8 @@ resource "aws_s3_bucket" "artifacts-bucket" {
 resource "aws_s3_bucket" "static-web-bucket" {
   bucket = var.static_web_bucket_name
   acl    = "private"
+
+  force_destroy = true
 
   tags = {
     Name        = var.static_web_bucket_name
